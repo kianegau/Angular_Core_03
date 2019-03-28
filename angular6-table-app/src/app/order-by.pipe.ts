cbcf78c433d47cce9236fceb10f4ceb78bc1c97e
kianegau <kianeganu@gmail.com>
2019-03-28 10:38:05 +0700

    update-order-by-2804

--- angular6-table-app/src/app/order-by.pipe.ts
+++ angular6-table-app/src/app/order-by.pipe.ts
@@ -6,6 +6,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class OrderByPipe implements PipeTransform {
   transform(array: any[], field: string): any[] {
     array.sort((a: any, b: any) => {
+      if (typeof a[field] === 'string') {
+        if (a[field].length < b[field].length) {
+          return -1;
+        }
+        if (a[field].length > b[field].length) {
+          return 1;
+        }
+        if (a[field].length === a[field].length) {
+          return a[field].localeCompare(b[field]);
+        }
+        return a[field].localeCompare(b[field]);
+      }
       if (a[field] < b[field]) {
         return -1;
       } else if (a[field] > b[field]) {
